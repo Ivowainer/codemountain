8178e3b92e7395f422560fe0f79bb5683652057e
Ivowainer <ivancamposwainer@gmail.com>
2022-11-15 17:37:23 -0300

    Header Hey!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import { MainLayout } from '../src/components';
 import Image from 'next/image';
-import MountainDraw from '../src/components/UI/MountainDraw';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <MainLayout pageDescription='About the page'>
       <div className="flex header w-screen justify-between px-40 items-center">
-        <div className="flex text-7xl leading-tight text-indigo-400 line-h font-bold flex-col w-1/2">
-          <p>Hey!</p>
+        <div className="flex text-7xl leading-tight text-sky-400 line-h font-bold flex-col w-1/2">
+          <p>Hey!<span className='text-6xl'>👋</span></p>
           <p>We are</p>
           <p className='bg-sky-500 text-white px-3 py-1 text-center'>CodeMountain</p>
           <p className='mt-5 leading-tight font-medium text-xl w-5/6 text-gray-500'>We provide IT solutions for companies that are looking for a digital transformation, both internal and externally.</p>
         </div>
-        {/* <MountainDraw /> */}
-        <div className="rounded-full">
+        <div className="rounded-full mountain-animation">
           <Image className='rounded-full' src='/Mountain.png' width={400} height={400} alt="Mountain" />
         </div>
       </div>
--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 export const NavBar = () => {
   return (
     <div className='bg-white px-16 py-6 flex justify-between items-center'>
       {/* <Image src='/Logo.png' width={55} height={110} alt=''/> */}
-      <p className='text-center font-bold text-2xl text-sky-400'>Code<span className='text-sky-400'>Mountain</span></p>
+      <Link href="/" className='cursor-pointer text-center font-bold text-2xl text-sky-400'>Code<span className='text-sky-400'>Mountain</span></Link>
       <div className="ml-5 flex gap-14 justify-center items-center">
         <p className='text-gray-400 font-normal cursor-pointer ink link-underline link-underline-black'>Products</p>
         <p className='text-gray-400 font-normal cursor-pointer ink link-underline link-underline-black'>Pricing</p>
